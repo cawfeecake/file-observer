1605fab6eea521a3fc0ea60f9624ecb2e4d56774
cawfeecake <cawfeecake@icloud.com>
2023-04-02 18:53:20 -0700

    rework how templating, data is handled; how build happens

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-build:
-	mkdir _site
+build: clean
+	mkdir -p _site
+	# TODO have index.html ref all htmls for each *file* of *files*
+	# note: order after ./generate_files_html.js call?
 	cp index.html _site/
 	cp styles.css _site/
-	cat templates/files.html | ./generate_files_html.rb template_data.json > _site/random.html
+	#cat templates/diffs.html | ./generate_files_html.js > _site/diffs.html
+	jq '.files[0]' input-files.json | ./generate_file_diff_json.js > /tmp/tmp.json
+	cat /tmp/tmp.json | ./generate_file_diff_html.js > _site/diffs.html
+
 .PHONY: build
 
 clean:
 	rm -rf _site
+	rm -rf __tmp_*/
 .PHONY: clean
---
+++ generate_file_diff_json.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process')
+const { mkdtempSync, readFileSync, writeFileSync } = require('fs')
+const { basename } = require('path')
+
+
+// usage: takes in JSON with the following properties:
+// - "filepath": the path to a file relative to a repository's root
+// - "repos": an array ([ ... ]) of names of the repos from which to get copies of the file
+const { filepath, repos } = JSON.parse(
+    readFileSync(process.stdin.fd, 'utf-8')
+)
+
+const reposByContent = new Map() // collects repos together that have equal content
+const uniqRepos = [...new Set(repos)]
+uniqRepos.map(repoName => {
+  const getEncodedContentCmd = `gh api repos/cawfeecake/${repoName}/contents/${filepath} --jq '.content'`
+  const encodedContent = execSync(getEncodedContentCmd, { shell: '/bin/bash' }).toString().replace(/\n/g, '')
+  if (reposByContent.has(encodedContent)) {
+    const reposWithSameContent = reposByContent.get(encodedContent)
+    reposByContent.set(encodedContent, [...reposWithSameContent, repoName])
+  } else {
+    reposByContent.set(encodedContent, [repoName])
+  }
+})
+
+// at this point, we have done all necessary calls to GitHub (w/o double requests)
+
+const AnsiConverter = require('ansi-to-html')
+
+
+const ansiConverter = new AnsiConverter()
+const tmpDir = mkdtempSync(`__tmp_${basename(__filename)}`)
+let count = 0
+const filepathToCompareAgainst = `${tmpDir}/${count}`
+const htmlTemplateInput = [...reposByContent.entries()].sort((a, b) => b[1].length - a[1].length).map(([content, repos]) => {
+  // in order from most repos to lease
+  const file = Buffer.from(content, 'base64').toString('ascii')
+  const filepath = `${tmpDir}/${count}`
+  writeFileSync(filepath, file)
+
+  let diffContent
+  if (count++ == 0) {
+    diffContent = file
+  } else { // count > 0
+    try {
+      execSync(`git diff --color=always --no-index ${filepathToCompareAgainst} ${filepath}`, { shell: '/bin/bash' })
+    } catch (err) {
+      // .status: cmd's exit code
+      // .message: reason for the error
+      // .stderr.toString(): ???
+      // .stdout.toString(): what the cmd had sent to output before error
+      diffContent = err.stdout.toString()
+    }
+    if (!diffContent) {
+      throw new Error('`git diff ...` returned no differences which is unexpected!');
+    }
+  }
+
+  return {
+    repos,
+    content: ansiConverter.toHtml(diffContent)
+  }
+})
+console.log(JSON.stringify({
+    filepath,
+    diffs: htmlTemplateInput,
+}))
